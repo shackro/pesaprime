cbfd27566759355f98bfdc694ae3b9f4c6d9cbc5
Shadrack <128532446+shackro@users.noreply.github.com>
2025-11-20 22:53:51 -0500

    Add base path for Vercel deployments
    Set base path for Vercel deployments.

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",  // <-- REQUIRED FOR VERCEL DEPLOYMENTS
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
