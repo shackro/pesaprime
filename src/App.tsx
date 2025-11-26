838d9a92703f46ab8b86f96caac9b0ee50ad2d70
Shadrack <128532446+shackro@users.noreply.github.com>
2025-11-26 03:50:33 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,20 @@ import FAQs from './pages/FAQs';
 import Contact from './pages/Contact';
 import AboutUs from './pages/AboutUs';
 import Trading from './components/Trading';
-import InvestmentsPage from "./pages/InvestmentsPage";
+// import InvestmentsPage from "./pages/InvestmentsPage";
 
 // Loading component
 const LoadingSpinner = () => (
-  <div className="min-h-screen flex items-center justify-center">
-    <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-emerald-600"></div>
-  </div>
+      <div style="position: fixed; top: -2; left: -2; width: 100%; height: 100%; background: #4b5563; display: flex; justify-content: center; align-items: center; z-index: 9999;" class="dark:bg-gray-900">
+        <div class="relative text-center items-center justify-center">
+          <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" fill="currentColor" class="fill-green-600" viewBox="0 0 16 16">
+            <path d="M5.5 9.511c.076.954.83 1.697 2.182 1.785V12h.6v-.709c1.4-.098 2.218-.846 2.218-1.932 0-.987-.626-1.496-1.745-1.76l-.473-.112V5.57c.6.068.982.396 1.074.85h1.052c-.076-.919-.864-1.638-2.126-1.716V4h-.6v.719c-1.195.117-2.01.836-2.01 1.853 0 .9.606 1.472 1.613 1.707l.397.098v2.034c-.615-.093-1.022-.43-1.114-.9zm2.177-2.166c-.59-.137-.91-.416-.91-.836 0-.47.345-.822.915-.925v1.76h-.005zm.692 1.193c.717.166 1.048.435 1.048.91 0 .542-.412.914-1.135.982V8.518z"/>
+            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+            <path d="M8 13.5a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11m0 .5A6 6 0 1 0 8 2a6 6 0 0 0 0 12"/>
+          </svg>
+          <div class="absolute -top-3 -left-3 h-20 w-20 rounded-full border-8 border-dotted border-green-600 animate-spin-slow dark:border-green-400"></div>
+        </div>
+      </div>
 );
 
 // Protected Route component
@@ -87,13 +94,13 @@ function AppRoutes() {
           </BaseLayout>
         </ProtectedRoute>
       } />
-      <Route path="/investments" element={
+      {/* <Route path="/investments" element={
         <ProtectedRoute>
           <BaseLayout>
             <InvestmentsPage />
           </BaseLayout>
         </ProtectedRoute>
-      } />
+      } /> */}
       <Route path="/deposit" element={
         <ProtectedRoute>
           <BaseLayout>
